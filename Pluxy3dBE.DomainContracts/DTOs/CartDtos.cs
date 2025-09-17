f9d2cc629f89bc5bf7f1074a0dd5d3e00bcf0715
OmarMusmanno <omarezequiel.musmanno@alumnos.uai.edu.ar>
2025-09-16 21:55:38 -0300

    chore: remove duplicate CartDtos (use existing CarritoDto.UpdateCarritoItemDto)

--- Pluxy3dBE.DomainContracts/DTOs/CartDtos.cs
+++
@@ -1,8 +0,0 @@
-namespace Pluxy3dBE.DomainContracts.DTOs
-{
-    public record UpdateCarritoItemDto
-    {
-        public int ItemId { get; init; }
-        public int NuevaCantidad { get; init; }
-    }
-}
